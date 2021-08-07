25d53afa9d3366cceda68f26b06a0f11292641a6
adg202 <qkrtpwls331@kyonggi.ac.kr>
2021-08-07 17:43:36 +0900

    .

---
+++ BOJ_s5_1476.py
@@ -0,0 +1,21 @@
+E, S, M = map(int, input().split())
+
+n = 1
+while True:
+    a = n % 15
+    if a == 0:
+        a = 15
+
+    b = n % 28
+    if b == 0:
+        b = 28
+
+    c = n % 19
+    if c == 0:
+        c = 19
+
+    if a == E and b == S and c == M:
+        print(n)
+        break
+    else:
+        n += 1
